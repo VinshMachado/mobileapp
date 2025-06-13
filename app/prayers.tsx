f7428d6e8947a0f074742ef5dd549b5ad04d6570
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-06-14 00:34:39 +0530

    buttons

---
+++ app/prayers.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const prayers = () => {
+  return (
+    <View>
+      <Text>prayers</Text>
+    </View>
+  );
+};
+
+export default prayers;
